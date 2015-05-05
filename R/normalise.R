ba4efe813ae00c91679579cef1487fe4008c1247
Jo-fai Chow <jofai.chow@gmail.com>
2015-05-05 16:17:41 +0100

    first commit

---
+++ R/normalise.R
@@ -0,0 +1,20 @@
+#' Normalise a data frame or matrix
+#'
+#' This function normalise a data frame or matrix.
+#'
+#' @param x a data frame or matrix.
+#' @param method 'range': normalise to values between 0 and 1.
+#'
+#' @examples
+#' ## Normalise a matrix
+#' x <- matrix(rnorm(100), nrow = 10)
+#' x_norm <- normalise(x)
+#' print(summary(x))
+#' print(summary(x_norm))
+#'
+#' @export
+
+normalise <- function(x, method = "range") {
+  pp <- preProcess(x, method)
+  return(predict(pp, x))
+}
